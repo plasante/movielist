2eccc515fb6eb4e96a537881257886e8a0cb09d4
Pierre Lasante <pierre.lasante@videotron.ca>
2012-01-15 13:31:51 -0500

    Next I have to implement users edit/update

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -25,5 +25,41 @@ describe "Users" do
         controller.should_not be_signed_in
       end
     end
+  end # of describe signin/signout
+  
+  describe "signup" do
+    describe "signup failure" do
+      it "should not create a user" do
+        lambda do
+          visit signup_path
+          fill_in :user_first_name, :with => ""
+          fill_in :user_last_name,  :with => ""
+          fill_in :user_username,   :with => ""
+          fill_in :user_email,      :with => ""
+          fill_in :user_password,   :with => ""
+          fill_in :user_password_confirmation, :with => ""
+          click_button
+        end.should_not change(User, :count)
+        controller.should_not be_signed_in
+      end
+    end
+    
+    describe "signup success" do
+      it "should create a user" do
+        lambda do
+          visit signup_path
+          fill_in :user_first_name, :with => "Pierre"
+          fill_in :user_last_name,  :with => "Lasante"
+          fill_in :user_username,   :with => "plasante"
+          fill_in :user_email,      :with => "plasante@email.com"
+          fill_in :user_password,   :with => "123456"
+          fill_in :user_password_confirmation, :with => "123456"
+          click_button
+        end.should change(User, :count).by(1)
+        controller.should be_signed_in
+        response.should render_template("users/show")
+        response.should have_selector("div.flash.notice", :content => "Welcome")
+      end
+    end
   end
 end
